7a48d4b549601afdfbbd7c223c36c55262e2f548
Sarah Possidônio <115196872+sarahpossidonio@users.noreply.github.com>
2023-12-04 10:04:14 -0300

    Commit Inicial
    Commit Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {IMaskModule} from 'angular-imask';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {Observable, map, startWith} from 'rxjs';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule,
+    RouterOutlet, MatFormFieldModule, MatInputModule,
+    MatSelectModule, MatCardModule, FlexLayoutModule,
+    MatSliderModule, MatToolbarModule, FormsModule, IMaskModule,
+    ReactiveFormsModule, MatAutocompleteModule,
+  MatIconModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  formulario:FormGroup = new FormGroup({
+    saida: new FormControl(''),
+    destino: new FormControl(''),
+    veiculo: new FormControl(''),
+    valCombustivel: new FormControl(''),
+    qtdParadas: new FormControl (0),
+    qtdPessoas: new FormControl (1),
+    valAlimentacao: new FormControl ('')
+  })
+
+  myControl = new FormControl('');
+  filteredOptions: Observable<string[]> | undefined;
+  options: string[] = ["Alegrete","Barra do Quaraí","Garruchos",
+  "Itaqui","Maçambará","Manoel Viana","Quaraí","São Borja",
+  "São Francisco de Assis","Uruguaiana","Rosário do Sul",
+  "Santa Margarida do Sul","Sant'Ana do Livramento","São Gabriel",
+  "Aceguá","Bagé","Dom Pedrito","Hulha Negra","Lavras do Sul"];
   title = 'a3-front';
+
+  valorCombustivel: string = "";
+
+  mask = {
+    mask: Number,  // enable number mask
+
+    // other options are optional with defaults below
+    scale: 2,  // digits after point, 0 for integers
+    thousandsSeparator: '.',  // any single char
+    padFractionalZeros: true,  // if true, then pads zeros at end to the length of scale
+    normalizeZeros: false,  // appends or removes zeros at ends
+    radix: ',',  // fractional delimiter
+    mapToRadix: ['.'],  // symbols to process as radix
+
+    // additional number interval options (e.g.)
+    min: 0,
+    max: 100000
+  }
+
+  ngOnInit(): void {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+
+
+  }
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+  onSubmit(){
+    console.log(this.formulario);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideNgxMask } from 'ngx-mask';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimations(),
+  provideNgxMask()]
 };
